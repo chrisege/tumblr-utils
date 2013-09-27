1ac320631c9b3f0009440c2b72ab9339670a9e36
Chris Ege <chrisege@gmail.com>
2013-09-27 11:19:51 -0500

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,7 @@
+module.exports = function(grunt) {
+	// load all grunt tasks matching the `grunt-*` pattern
+	require('load-grunt-tasks')(grunt);
+
+	grunt.initConfig();
+	grunt.registerTask('default', []);
+};
