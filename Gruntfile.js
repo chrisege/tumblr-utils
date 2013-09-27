fcd95426d1deadf8a6960e27be614149e703683c
Chris Ege <chrisege@gmail.com>
2013-09-27 12:25:19 -0500

    watch tasks, jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,8 +2,33 @@ module.exports = function(grunt) {
 	// load all grunt tasks matching the `grunt-*` pattern
 	require('load-grunt-tasks')(grunt);
 
-	grunt.initConfig();
-	
+	grunt.initConfig({
+		jshint: {
+			all: ['Gruntfile.js', 'app.js']
+		},
+		nodemon: {
+			dev: {
+				options: {
+					file: 'app.js'
+				}
+			}
+		},
+		watch: {
+			mainApp: {
+				files: ['app.js'],
+				tasks: ['jshint']
+            } 
+		},
+		concurrent: {
+			dev: {
+				options: {
+					logConcurrentOutput: true
+				},
+				tasks: ['watch', 'nodemon:dev']
+			}
+		},
+	});
+
 	grunt.registerTask('default', []);
-	grunt.registerTask('dev', ['build:debug', 'concurrent:dev']);
+	grunt.registerTask('dev', ['concurrent:dev']);
 };
--- app.js
+++ app.js
@@ -1,68 +1,68 @@
 var express = require('express'),
-    sys = require('sys'),
-    oauth = require('oauth'),
-    tumblr = require('./tumblr-keys.js');
+		sys = require('sys'),
+		oauth = require('oauth'),
+		tumblr = require('./tumblr-keys.js');
 
 var app = express();
 
 var consumer = function() {
 	return new oauth.OAuth(
-    	"https://tumblr.com/oauth/request_token", "http://www.tumblr.com/oauth/access_token", 
-    	tumblr.key, tumblr.secret, "1.0A", "http://127.0.0.1:3000/sessions/callback/", "HMAC-SHA1"
-    );   
+			"https://tumblr.com/oauth/request_token", "http://www.tumblr.com/oauth/access_token", 
+			tumblr.key, tumblr.secret, "1.0A", "http://127.0.0.1:3000/sessions/callback/", "HMAC-SHA1"
+		);   
 };
 
 app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-  app.use(express.logger());
-  app.use(express.cookieParser('hello'));
-  app.use(express.session());
-  app.use(function(req, res, next){
-  	res.locals.session = req.session;
-  	next();
-  });
+	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+	app.use(express.logger());
+	app.use(express.cookieParser('hello'));
+	app.use(express.session());
+	app.use(function(req, res, next){
+		res.locals.session = req.session;
+		next();
+	});
 });
 
 app.get('/', function(req, res){
-  var body = 'Hello World'+tumblr.key;
-  res.setHeader('Content-Type', 'text/plain');
-  res.setHeader('Content-Length', body.length);
-  res.end(body);
+	var body = 'Hello World'+tumblr.key;
+	res.setHeader('Content-Type', 'text/plain');
+	res.setHeader('Content-Length', body.length);
+	res.end(body);
 });
 
 app.get('/sessions/connect', function(req, res){
-  consumer().getOAuthRequestToken(function(error, oauthToken, oauthTokenSecret, results){
-    if (error) {
-      res.send("Error getting OAuth request token : " + sys.inspect(error), 500);
-    } else {  
-      req.session.oauthRequestToken = oauthToken;
-      req.session.oauthRequestTokenSecret = oauthTokenSecret;
-      res.redirect("http://www.tumblr.com/oauth/authorize?oauth_token="+req.session.oauthRequestToken);      
-    }
-  });
+	consumer().getOAuthRequestToken(function(error, oauthToken, oauthTokenSecret, results){
+		if (error) {
+			res.send("Error getting OAuth request token : " + sys.inspect(error), 500);
+		} else {  
+			req.session.oauthRequestToken = oauthToken;
+			req.session.oauthRequestTokenSecret = oauthTokenSecret;
+			res.redirect("http://www.tumblr.com/oauth/authorize?oauth_token="+req.session.oauthRequestToken);      
+		}
+	});
 });
 
 app.get('/sessions/callback', function(req, res){
-  sys.puts(">>"+req.session.oauthRequestToken);
-  sys.puts(">>"+req.session.oauthRequestTokenSecret);
-  sys.puts(">>"+req.query.oauth_verifier);
-  consumer().getOAuthAccessToken(req.session.oauthRequestToken, req.session.oauthRequestTokenSecret, req.query.oauth_verifier, function(error, oauthAccessToken, oauthAccessTokenSecret, results) {
-    if (error) {
-      res.send("Error getting OAuth access token : " + sys.inspect(error) + "["+oauthAccessToken+"]"+ "["+oauthAccessTokenSecret+"]"+ "["+sys.inspect(results)+"]", 500);
-    } else {
-      req.session.oauthAccessToken = oauthAccessToken;
-      req.session.oauthAccessTokenSecret = oauthAccessTokenSecret;
-      // Right here is where we would write out some nice user stuff
-      consumer.get("http://api.tumblr.com/v2/user/info", req.session.oauthAccessToken, req.session.oauthAccessTokenSecret, function (error, data, response) {
-        if (error) {
-          res.send("Error getting tumblr data : " + sys.inspect(error), 500);
-        } else {
-          req.session.tumblrname = data.name;    
-          res.send('You are signed in: ' + req.session.tumblrname)
-        }  
-      });  
-    }
-  });
+	sys.puts(">>"+req.session.oauthRequestToken);
+	sys.puts(">>"+req.session.oauthRequestTokenSecret);
+	sys.puts(">>"+req.query.oauth_verifier);
+	consumer().getOAuthAccessToken(req.session.oauthRequestToken, req.session.oauthRequestTokenSecret, req.query.oauth_verifier, function(error, oauthAccessToken, oauthAccessTokenSecret, results) {
+		if (error) {
+			res.send("Error getting OAuth access token : " + sys.inspect(error) + "["+oauthAccessToken+"]"+ "["+oauthAccessTokenSecret+"]"+ "["+sys.inspect(results)+"]", 500);
+		} else {
+			req.session.oauthAccessToken = oauthAccessToken;
+			req.session.oauthAccessTokenSecret = oauthAccessTokenSecret;
+			// Right here is where we would write out some nice user stuff
+			consumer.get("http://api.tumblr.com/v2/user/info", req.session.oauthAccessToken, req.session.oauthAccessTokenSecret, function (error, data, response) {
+				if (error) {
+					res.send("Error getting tumblr data : " + sys.inspect(error), 500);
+				} else {
+					req.session.tumblrname = data.name;    
+					res.send('You are signed in: ' + req.session.tumblrname);
+				}  
+			});  
+		}
+	});
 });
 
 app.listen(3000);
